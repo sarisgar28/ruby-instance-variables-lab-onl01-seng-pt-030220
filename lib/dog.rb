31f77c00f41fde121da890d380dfbdabaf3b6db9
Sara Isabel Garcia Reyes <sarisgar28@gmail.com>
2020-03-19 04:12:16 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,4 +4,12 @@ class Dog
 def name(dogs_name)
 @this_dogs_name = dogs_name
   end 
-end   
+  def name 
+    @this_dogs_name
+  end   
+end 
+
+lassie = Dog.new 
+lassie.new = "lassie"
+
+puts lassie.name 
